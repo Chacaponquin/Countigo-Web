c2159083e382d1de8a615afcf73d0b0fa4e2573b
Chacaponquin <hectorangel2001@gmail.com>
2022-05-14 18:07:43 -0400

    Responsive view Article

--- src/app/modules/blog/containers/article/components/comment-card/comment-card.component.ts
+++ src/app/modules/blog/containers/article/components/comment-card/comment-card.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class CommentCardComponent implements OnInit {
   constructor() {}
 
-  replies = [1, 2, 3];
+  replies = [1, 2];
 
   ngOnInit(): void {}
 }
